b459ebcc2835b78de19b180e1db98d6a961f07cf
Vikas Rathod <vikas.rv4299@gmail.com>
2021-05-24 20:24:36 +0530

    Counter

---
+++ app.js
@@ -0,0 +1,24 @@
+
+function myFunction()
+{
+var x = document.getElementById('value');
+var f=Number(x.innerHTML)+1
+x.innerHTML=f
+}
+
+
+function myFunction2()
+{
+var x = document.getElementById('value');
+var f=Number(x.innerHTML)*0
+x.innerHTML=f
+}
+
+function myFunction3()
+{
+var x = document.getElementById('value');
+var f=Number(x.innerHTML)-1
+x.innerHTML=f
+}
+
+
